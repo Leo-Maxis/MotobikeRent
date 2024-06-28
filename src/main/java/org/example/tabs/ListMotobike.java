2e853a97ad1c3f2cca98490cfc6abc46e1a3a123
Leo-Maximus <leo.longlhb@gmail.com>
2024-06-28 18:06:24 +0700

    add list motobike form

---
+++ src/main/java/org/example/tabs/ListMotobike.java
@@ -0,0 +1,17 @@
+package org.example.tabs;
+
+import javax.swing.*;
+
+public class ListMotobike {
+    private JTextField txtIdBike;
+    private JTextField txtNameBike;
+    private JTextField txtYearModelBike;
+    private JComboBox cboTypeBike;
+    private JFormattedTextField txtPriceBike;
+    private JTable tblListMotobike;
+    private JButton btnDelete;
+    private JButton btnUpdate;
+    private JButton btnSave;
+    private JButton btnEdit;
+    private JButton btnNew;
+}
