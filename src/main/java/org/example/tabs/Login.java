469e8e92f5a95fca0cb8d166f4c1b1fb95bb65da
Leo-Maximus <leo.longlhb@gmail.com>
2024-06-28 16:30:32 +0700

    add login form

---
+++ src/main/java/org/example/tabs/Login.java
@@ -0,0 +1,14 @@
+package org.example.tabs;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JButton cancelButton;
+    private JButton loginButton;
+    private JCheckBox rememberMeCheckBox;
+
+
+
+}
